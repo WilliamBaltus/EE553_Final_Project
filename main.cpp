21900b02ae9e023eb148c7afcd397208015b5164
WilliamBaltus <wbaltus21@gmail.com>
2022-04-21 20:21:24 -0400

    added functions

--- main.cpp
+++ main.cpp
@@ -5,20 +5,24 @@
 
 using namespace std;
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+static int callback(void *data, int argc, char **argv, char **azColName){
     int i;
-    for(i = 0; i<argc; i++) {
+    fprintf(stderr, "%s: ", (const char*)data);
+
+    for(i = 0; i<argc; i++){
         printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
     }
+
     printf("\n");
     return 0;
-};
+}
 
 int main(int argc, char* argv[]) {
     sqlite3* DB;
     char *sql;
     char *zErrMsg = 0;
     int db_curr = 0;
+    const char* data = "Print Function Called... ";
 
     db_curr =  sqlite3_open_v2("C:\\Users\\wbalt\\CLionProjects\\Final_Project\\university.db",
                                    &DB, SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE, NULL);
@@ -31,13 +35,18 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Opened database successfully\n");
     }
 
+
+    string tableName;
+    //cin >> tableName;
+    //string sqlStmt = "CREATE TABLE " + tableName + "(";
+
     /* Create SQL statement */
-    sql = "DROP TABLE IF EXISTS COMPANY;"
-          "CREATE TABLE COMPANY("  \
+    sql = "DROP TABLE IF EXISTS STEVENS;"
+          "CREATE TABLE STEVENS("  \
       "ID INT PRIMARY KEY     NOT NULL," \
       "NAME           TEXT    NOT NULL," \
       "AGE            INT     NOT NULL," \
-      "ADDRESS        CHAR(50)," \
+      "PROFESSION        CHAR(50)," \
       "SALARY         REAL );";
 
     /* Execute SQL statement */
@@ -50,15 +59,15 @@ int main(int argc, char* argv[]) {
         fprintf(stdout, "Table created successfully\n");
     }
 
-    /* Create SQL statement */
-    sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
-         "VALUES (1, 'Paul', 32, 'California', 20000.00 ); " \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
-         "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); "     \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
-         "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
-         "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+    /* INSERT SQL statement */
+    sql = "INSERT INTO STEVENS (ID,NAME,AGE,PROFESSION,SALARY) "  \
+         "VALUES (1, 'William', 21, 'Student', 0.00 ); " \
+         "INSERT INTO STEVENS (ID,NAME,AGE,PROFESSION,SALARY) "  \
+         "VALUES (2, 'Julia', 21, 'Student', 0.00 ); "     \
+         "INSERT INTO STEVENS (ID,NAME,AGE,PROFESSION,SALARY)" \
+         "VALUES (3, 'Yousef', 31, 'Professor', 10.00 );" \
+         "INSERT INTO STEVENS (ID,NAME,AGE,PROFESSION,SALARY)" \
+         "VALUES (4, 'Alhussain', 25, 'Admin', 69000.00 );";
 
     /* Execute SQL statement */
     db_curr = sqlite3_exec(DB, sql, callback, 0, &zErrMsg);
@@ -70,6 +79,38 @@ int main(int argc, char* argv[]) {
         fprintf(stdout, "Records created successfully\n");
     }
 
+    /* Create SQL statement */
+    sql = "SELECT * from STEVENS";
+
+    /* Execute SQL statement */
+    db_curr = sqlite3_exec(DB, sql, callback, (void*)data, &zErrMsg);
+
+    if( db_curr != SQLITE_OK ) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+
+
+    /* Delete SQL statement */
+    sql = "DELETE from STEVENS where ID=2; " \
+         "SELECT * from STEVENS";
+    /* Execute SQL statement */
+    db_curr = sqlite3_exec(DB, sql, callback, (void*)data, &zErrMsg);
+
+
+    cout << "----------------------------------------------------" << endl;
+    cout << "DELETING FROM TABLE " << endl;
+    if( db_curr != SQLITE_OK ) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+
+
+    // CLOSE SQL
     sqlite3_close(DB);
     return (0);
 }
